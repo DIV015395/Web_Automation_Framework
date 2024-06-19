7ca2cc7e775aa373ad4d16c2cdde7e3c9cf7b8eb
ManjeetSharma <sharmaboymanjeet12@gmail.com>
2024-06-20 00:06:26 +0530

    Web Automation Framework

---
+++ src/main/java/org/example/com/actions/Actions.java
@@ -0,0 +1,33 @@
+package org.example.com.actions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class Actions {
+	
+	WebDriver driver;
+	
+	public Actions(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void click(WebElement element) {
+		element.click();
+	}
+	
+	public void enterText(WebElement inputfield, String value) {
+		inputfield.sendKeys(value);
+	}
+	
+	public void navigateTo(String url) {
+		driver.get(url);
+		
+	}
+	
+	public void selectFromDropdown(WebElement dropdown, String value) {
+		Select dd = new Select(dropdown);
+		dd.selectByValue(value);
+	}
+
+}
---
+++ src/test/java/org/example/ValidateFooter.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import com.actions.Actions;
+import com.base.Base;
+import com.pageObjects.HomePage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class ValidateFooter extends Base {
+	
+	public WebDriver driver;
+	public Actions actions;
+	
+	private Logger log = LogManager.getLogger(ValidateFooter.class.getName());
+	
+	@BeforeTest
+	public void initialize() throws IOException {
+		driver = initializeDriver();
+		log.info("Driver is initialized.");
+		actions = new Actions(driver);
+		actions.navigateTo(prop.getProperty("url"));
+	}
+	
+	@Test
+	public void checkFooter() throws IOException {
+
+		HomePage hp = new HomePage(driver);
+		
+		Assert.assertTrue(hp.getFooter().isDisplayed());
+		log.info("Footer is displayed");
+
+	}
+	
+	@AfterTest
+	public void teardown() {
+		driver.close();
+		log.info("Driver is closed");
+	}
+}
